073e658a418f3ff328fcce41c39bf0ff3fa83698
azuman <hazimetedesu@gmail.com>
2019-09-29 18:51:11 +0900

    Add Report index

---
+++ src/controllers/reports/ReportsIndexServlet.java
@@ -0,0 +1,69 @@
+package controllers.reports;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
+
+//EmployeesIndexServletとほぼ一緒
+/**
+ * Servlet implementation class ReportsIndexServlet
+ */
+@WebServlet("/reports/index")
+public class ReportsIndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportsIndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page = 1;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) { }
+        List<Employee> reports = em.createNamedQuery("getAllReports", Employee.class)
+                                     .setFirstResult(15 * (page - 1))
+                                     .setMaxResults(15)
+                                     // 結果をgetResultList()メソッドで、リスト形式で取得
+                                     .getResultList();
+
+        // 全件数を取得
+        long reports_count = (long)em.createNamedQuery("getReportsCount", Long.class)
+                                       .getSingleResult();
+
+        em.close();
+
+        // リクエストスコープ(controller)にセット
+        request.setAttribute("reports", reports);
+        request.setAttribute("reports_count", reports_count);
+        request.setAttribute("page", page);
+
+        //セッションにフラッシュがあったら、フラをセッションからリクエストに移動し、セッションからは削除。
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/index.jsp");
+        rd.forward(request, response);
+    }
+}
